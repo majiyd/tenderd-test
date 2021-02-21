861eda858d871075e8ee0fd6fe84444d32b6114c
Majiyd <majeed@patricia.com.ng>
2021-02-21 09:14:16 +0100

    Implement create user

---
+++ functions/controllers/users/create.js
@@ -0,0 +1,54 @@
+const firebase = require("firebase/app");
+require("firebase/auth");
+// require("firebase/firestore");
+const Joi = require("@hapi/joi");
+const db = require("../../db");
+
+
+module.exports = function(req, res) {
+  try {
+    const {email, password} = req.body;
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    });
+
+    const {error, value} = schema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({message: error.message});
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+          // Signed in
+
+          const {email, uid} = userCredential.user;
+
+          const data = {
+            name: value.name,
+            email,
+            createdAt: Date.now(),
+            companyID: "",
+            uuid: uid,
+          };
+
+          // create user
+
+          db.collection("users").doc(data.uuid)
+              .set(data)
+              .then(()=> {
+                return res.status(201).json(data);
+              }).catch((err) => {
+                return res.status(500).json({message: err.message});
+              });
+        })
+        .catch((error) => {
+          return res.status(500).json({message: error.message});
+        });
+  } catch (error) {
+    return res.status(500).json({message: error.message});
+  }
+};
+
--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,26 @@
 const functions = require("firebase-functions");
+const firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors({origin: true}));
 
-const companies = require("./controllers/companies");
+const serviceAccount = require("./permissions.json");
+
+firebase.initializeApp(serviceAccount);
+
+const Companies = require("./controllers/companies");
+const Users = require("./controllers/users");
 
 // companies
-app.post("/companies", companies.create);
-app.get("/companies", companies.getAll);
-app.get("/companies/:uuid", companies.getOne);
+app.post("/companies", Companies.create);
+app.get("/companies", Companies.getAll);
+app.get("/companies/:uuid", Companies.getOne);
 
 // users
+app.post("/users", Users.create);
 
 // 404
 app.use(function(req, res) {
